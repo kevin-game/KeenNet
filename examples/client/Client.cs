20174f89d634a47746c2ecf3c67fcf94879f88cf
ke152 <ke152@foxmail.com>
2023-04-21 23:50:56 +0800

    update: socket 1v1 communicate

---
+++ examples/client/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine($"[Client]");
+Socket client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+
+string msg = "This is a message from client";
+var buffer = Encoding.UTF8.GetBytes(msg);
+client.Send(buffer);
+
+Thread.Sleep(1000);
---
+++ examples/server/Server.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine($"[Server]");
+Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listener.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+listener.Listen();
+
+Socket client = listener.Accept();
+
+byte[] buffer = new byte[1024];
+int length = client.Receive(buffer);
+
+string msg = Encoding.UTF8.GetString(buffer, 0, length);
+Console.WriteLine($"{msg}");
+
+Thread.Sleep(1000);
